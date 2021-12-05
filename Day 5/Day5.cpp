0c9099d1cabf1388f28c57da5df3c97622a9a211
Matthew Li <m739li@uwaterloo.ca>
2021-12-05 02:24:29 -0500

    Added Day 5 Part 1

---
+++ Day 5/Day5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+
+int main(){
+    std::ifstream inFile;
+    int input[2000];
+    inFile.open(".\\input.txt");
+    int next_available{};
+    std::string temp{};
+    for (int i = 0; i < 2500; i++){
+        if ((i+3)%5 != 0){
+            inFile >> input[next_available];
+            next_available++;
+        } else{
+            inFile >> temp;
+        }
+    }
+    auto map = new int[1000][1000];
+    bool negative{false};
+    int number_of_marked{};
+    for (int i = 0; i < 2000; i = i + 4){
+        negative = false;
+        if (input[i] == input[i+2]){ // the x values are equal
+            number_of_marked = input[i+3] - input[i+1];
+            std::cout << "number of marked: " << number_of_marked << std::endl;
+            if (number_of_marked < 0){
+                number_of_marked = -number_of_marked;
+                number_of_marked += 1;
+                negative = true;
+            } else{
+                number_of_marked += 1;
+            }
+            std::cout << "number of marked after editing: " << number_of_marked << std::endl;
+            for (int a = 0; a < number_of_marked; a++){ // mark the map
+                if (negative == true){
+                    map[input[i+3]+a][input[i]] += 1;
+                } else{
+                    map[input[i+1]+a][input[i]] += 1;
+                }
+            }
+        } else if (input[i+1] == input[i+3]) { // the y values are equal
+            number_of_marked = input[i+2] - input[i];
+            if (number_of_marked < 0){
+                number_of_marked = -number_of_marked;
+                number_of_marked += 1;
+                negative = true;
+            } else{
+                number_of_marked += 1;
+            }
+            for (int a = 0; a < number_of_marked; a++){ // mark the map
+                if (negative == true){
+                    map[input[i+1]][input[i+2]+a] += 1;
+                } else{
+                    map[input[i+1]][input[i]+a] += 1;
+                }
+            }
+        }
+    }
+    int counter{};
+    int counter2{};
+    for (int i = 0; i < 1000; i++){
+        for (int j = 0; j < 1000; j++){
+            if (map[i][j] >= 2){
+                counter += 1;
+            } else if (map[i][j] > 0){
+                counter2 += 1;
+            }
+        }
+    }
+    std::cout << "Numbers greater than or equal to 2: " << counter << std::endl;
+    std::cout << "Numbers less than 2: " << counter2;
+    delete[] map;
+}
