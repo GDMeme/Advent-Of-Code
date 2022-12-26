40213b4aaaae1db0f7bb16772a0182f36a834eeb
Matthew Li <m739li@uwaterloo.ca>
2022-12-26 06:38:00 -0500

    Added Day 11 Part 1 and 2

---
+++ 2021/Day 11/Day11Part2.js
@@ -0,0 +1,89 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+
+let arr = syncReadFile('./input.txt');
+
+const directionArray = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+
+
+function flash (frontier, flashed, counter) {
+    while (frontier.length !== 0) {
+        counter++;
+        let [x, y] = frontier.shift().split(',').map(coordinate => parseInt(coordinate));
+        flashed.add(x + ',' + y);
+        for (const [directionX, directionY] of directionArray) {
+            const newCoordinateX = x + directionX;
+            const newCoordinateY = y + directionY;
+            if (newCoordinateX < 0) {
+                continue;
+            }
+            if (newCoordinateX >= arr[y].length) {
+                continue;
+            }
+            if (newCoordinateY < 0) {
+                continue;
+            }
+            if (newCoordinateY >= arr.length) {
+                continue;
+            }
+            if (parseInt(arr[newCoordinateY][newCoordinateX]) === 9) {
+                if (!flashed.has(newCoordinateX + ',' + newCoordinateY)) {
+                    if (!frontier.includes(newCoordinateX + ',' + newCoordinateY)) {
+                        frontier.push(newCoordinateX + ',' + newCoordinateY);
+                    }
+                }
+            } else {
+                arr[newCoordinateY] = setCharAt(arr[newCoordinateY], newCoordinateX, (parseInt(arr[newCoordinateY][newCoordinateX]) + 1).toString());
+            }
+
+        }
+    }
+    return counter;
+}
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+let counter = 0;
+
+let roundCounter = 0;
+
+while (true) {
+    let flashed = new Set();
+    let frontier = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            let currentNumber = parseInt(arr[i][j]);
+            if (currentNumber === 9) {
+                frontier.push(j + ',' + i);
+            }
+        }
+    }
+    for (let i = 0; i < arr.length; i++) { // increment them all at the same time, then check for flashes
+        for (let j = 0; j < arr[i].length; j++) { // check if any are nine, that means they are guaranteed to flash
+            let currentNumber = parseInt(arr[i][j]);
+            if (currentNumber !== 9) {
+                arr[i] = setCharAt(arr[i], j, (currentNumber + 1).toString());
+            }
+        }
+    }
+    counter = flash(frontier, flashed, counter);
+    for (const elem of flashed) {
+        const [x, y] = elem.split(',').map(coordinate => parseInt(coordinate));
+        arr[y] = setCharAt(arr[y], x, '0');
+    }
+    roundCounter++;
+    if (flashed.size === arr.length * arr[0].length) {
+        console.log('ans:', roundCounter);
+        break;
+    }
+}
