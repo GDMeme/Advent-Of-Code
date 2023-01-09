870a0a3beddea723f8c1da9b9a15ff0739843d72
Matthew Li <m739li@uwaterloo.ca>
2023-01-09 05:53:14 -0500

    Added Day 11 both parts, code looks really ugly

---
+++ 2020/Day 11/Day11Part2.js
@@ -0,0 +1,127 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+const {cloneDeep} = require('lodash');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+
+let arr = syncReadFile('./input.txt');
+
+let currentEmptySeatSet = new Set();
+
+let floorSet = new Set();
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] === 'L') {
+            currentEmptySeatSet.add([j, i].join());
+        } else {
+            floorSet.add([j, i].join());
+        }
+    }
+}
+
+let newEmptySeatSet = new Set();
+
+let currentOccupiedSeatSet = new Set();
+let newOccupiedSeatSet = new Set();
+
+const directionArray = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+
+while (true) {
+    for (const elem of currentEmptySeatSet) {
+        const [x, y] = elem.split(',').map(e => parseInt(e));
+        let found = false;
+        for (const direction of directionArray) {
+            const [directionX, directionY] = direction;
+            newX = x + directionX;
+            newY = y + directionY;
+            continueValue = false;
+            while (floorSet.has([newX, newY].join())) {
+                if (newX < 0 || newX > arr[0].length || newY < 0 || newY > arr.length) {
+                    continueValue = true;
+                    break;
+                }
+                newX += directionX;
+                newY += directionY;
+            }
+            if (continueValue) {
+                continue;
+            }
+            if (currentOccupiedSeatSet.has([newX, newY].join())) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            newOccupiedSeatSet.add(elem);
+        } else {
+            newEmptySeatSet.add(elem);
+        }
+    }
+    for (const elem of currentOccupiedSeatSet) {
+        const [x, y] = elem.split(',').map(e => parseInt(e));
+        let counter = 0;
+        for (const direction of directionArray) {
+            const [directionX, directionY] = direction;
+            newX = x + directionX;
+            newY = y + directionY;
+            let continueValue = false;
+            while (floorSet.has([newX, newY].join())) {
+                if (newX < 0 || newX > arr[0].length || newY < 0 || newY > arr.length) {
+                    continueValue = true;
+                    break;
+                }
+                newX += directionX;
+                newY += directionY;
+            }
+            if (continueValue) {
+                continue;
+            }
+            if (currentOccupiedSeatSet.has([newX, newY].join())) {
+                counter++;
+            }
+        }
+        if (counter >= 5) {
+            newEmptySeatSet.add(elem);
+        } else {
+            newOccupiedSeatSet.add(elem);
+        }
+    }
+    let noSeatChange = true;
+    if (newEmptySeatSet.size === currentEmptySeatSet.size) {
+        for (const elem of newEmptySeatSet) {
+            if (!currentEmptySeatSet.has(elem)) {
+                noSeatChange = false;
+                break;
+            }
+        }
+    } else {
+        noSeatChange = false;
+    }
+    if (noSeatChange && (newOccupiedSeatSet.size === currentOccupiedSeatSet.size)) {
+        for (const elem of newOccupiedSeatSet) {
+            if (!currentOccupiedSeatSet.has(elem)) {
+                noSeatChange = false;
+                break;
+            }
+        }
+    } else {
+        noSeatChange = false;
+    }
+    if (noSeatChange) {
+        break;
+    }
+    currentEmptySeatSet = cloneDeep(newEmptySeatSet);
+    currentOccupiedSeatSet = cloneDeep(newOccupiedSeatSet);
+
+    newEmptySeatSet.clear();
+    newOccupiedSeatSet.clear();
+}
+
+console.log(currentOccupiedSeatSet.size);
