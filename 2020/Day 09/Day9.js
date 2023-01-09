1aa7909bc795ed0c11780434f664f7f129c08671
Matthew Li <m739li@uwaterloo.ca>
2023-01-08 22:46:32 -0500

    Added Day 9 both parts

---
+++ 2020/Day 09/Day9.js
@@ -0,0 +1,27 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+const {cloneDeep} = require('lodash');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+
+let arr = syncReadFile('./input.txt').map(e => parseInt(e));
+
+let possibleSums = [];
+
+for (let i = 0; i < arr.length - 25; i++) {
+    for (let j = 0; j < 24; j++) {
+        for (let k = j + 1; k < 25; k++) {
+            possibleSums.push(arr[i + j] + arr[i + k]);
+        }
+    }
+    if (!possibleSums.includes(arr[i + 25])) {
+        break;
+    }
+    possibleSums.length = 0;
+}
---
+++ 2020/Day 09/Day9Part2.js
@@ -0,0 +1,28 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+const {cloneDeep} = require('lodash');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+
+let arr = syncReadFile('./input.txt').map(e => parseInt(e));
+
+const findNumber = 36845998;
+
+for (let i = 0; i < arr.length - 25; i++) {
+    let currentSum = 0;
+    let currentCounter = i;
+    while (currentSum < findNumber) {
+        currentSum += arr[currentCounter];
+        if (currentSum === findNumber && currentCounter !== i) {
+            console.log(arr[i] + arr[currentCounter]);
+            break;
+        }
+        currentCounter++;
+    }
+}
