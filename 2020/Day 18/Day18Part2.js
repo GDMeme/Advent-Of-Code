2449d176932dbebd708f1cb9dc3daf1946e2dd00
Matthew Li <m739li@uwaterloo.ca>
2023-01-12 15:27:14 -0500

    Added Day 18 both parts

---
+++ 2020/Day 18/Day18Part2.js
@@ -0,0 +1,50 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+
+let arr = syncReadFile('./input.txt');
+
+function evaluateExpression(expression) {
+    const expressionArray = expression.split(' ');
+    while (expressionArray.indexOf('+') !== -1) {
+        const currentIndex = expressionArray.indexOf('+');
+        const ans = parseInt(expressionArray[currentIndex - 1]) + parseInt(expressionArray[currentIndex + 1]);
+        expressionArray[currentIndex + 1] = ans;
+        expressionArray.splice(currentIndex - 1, 2);
+    }
+    let i = 1;
+    let ans = parseInt(expressionArray[0]);
+    while (i < expressionArray.length) {
+        if (expressionArray[i] === '+') {
+            ans += parseInt(expressionArray[i + 1]);
+        } else {
+            ans *= parseInt(expressionArray[i + 1]);
+        }
+        i += 2;
+    }
+    return ans;
+}
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] === ')') {
+            const startingIndex = arr[i].lastIndexOf('(', j);
+            const stringBefore = arr[i].slice(0, startingIndex);
+            const stringAfter = arr[i].slice(j + 1);
+            const expression = arr[i].slice(startingIndex + 1, j);
+            arr[i] = stringBefore + evaluateExpression(expression) + stringAfter;
+            j = 0;
+        }
+    }
+    sum += parseInt(evaluateExpression(arr[i]));
+}
+
+console.log(sum);
